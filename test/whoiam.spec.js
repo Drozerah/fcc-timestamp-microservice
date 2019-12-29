f17b479e9d69551917224e4024886c5fcb1b8d81
Drozerah <drozerah@gmail.com>
2019-12-29 01:11:35 +0100

    test: add api/whoiam testing
    - create ./test/whoiam.spec.js file
    - .eslintignore whoiam.spec.js file
    - create ./requests/api.whoiam.http file REST client file

---
+++ test/whoiam.spec.js
@@ -0,0 +1,24 @@
+const app = require("../app")
+const request = require("supertest")
+/**
+* Specifications 
+*/
+// ? [x] I can get the IP address, language and operating system for the browser.
+describe("Test API route /api/whoiam", () => {
+  it("It should retur the IP address, language and the and operating system for the browser", (done) => {
+    request(app)
+      .get("/api/whoiam")
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => {
+        if( response.body.ipaddress.length > 0 && 
+            response.body.language.length > 0 && 
+            response.body.software.length > 0 ){
+          done()
+        } else {
+          const e = 'error'
+          done(e)
+        }
+      })
+  })
+})
