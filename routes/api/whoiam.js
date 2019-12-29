166a597639a2c6c3e48b432d43dd932b0fb35876
Drozerah <drozerah@gmail.com>
2019-12-29 01:49:37 +0100

    fix: remove generated prefix from ip address

--- routes/api/whoiam.js
+++ routes/api/whoiam.js
@@ -3,8 +3,11 @@ const router = express.Router()
 
 /* GET whoiam */
 router.get('/', (req, res, next) => {
+  // remove prefix from ip
+  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  ip = ip.replace('::ffff:', '')
   res.json({
-    ipaddress: req.ip || 'undefined',
+    ipaddress: ip || 'undefined',
     language: req.headers['accept-language'] || 'undefined',
     software: req.headers['user-agent'] || 'undefined'
   })
