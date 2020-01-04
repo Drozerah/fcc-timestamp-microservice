c8dabc138cf4a272f5ced4d1302f69e703108378
Drozerah <drozerah@gmail.com>
2020-01-04 13:46:59 +0100

    refactor: Seeding Inital Data
    - refactor function names
    - add better comments

--- seeding/seeding.js
+++ seeding/seeding.js
@@ -7,58 +7,74 @@ const connectDB = require('../config/db')
 * NPM modules
 */
 const faker = require('faker')
-// const moment = require('moment') // not installed
+// const moment = require('moment') // ? not installed!
 /**
 * Mongoose Model
 */
 const Seed = require('./model/seeding.model') // seedings db collection model for testing
 const User = require('../models/exercicetracker') // users db collection model
-
 /**
-* Seeding with an array of documents
+* Array of documents
 */
-// @method          insertMany()
-const seed = function (documents) {
+const documents = []
+/**
+* Insert an Array of documents in collection
+*
+* @param {Array} documents the array of documents
+* @returns {Function} callback where db connection is closed after transaction
+*/
+const insertDocuments = (documents) => {
   // Insert Many documents
-  User.insertMany(documents, { ordered: false }, (err, seedings) => {
+  // @descritption    insert many dynamically created documents in db at once
+  // @method          .insertMany()
+  User.insertMany(documents, { ordered: false }, (err, docs) => {
+    // callBack
     if (err) {
       console.log(err)
     } else {
-      console.log(seedings)
+      console.log(docs)
       mongoose.connection.close()
       console.log('[MongoDB] connection closed!')
     }
   })
 }
 /**
-* Run 10 cycles creates 10 users
-* we use recursive function as loop
+* Fill an Array of documents to insert in db
+*
+* we use Faker.js to generate data
+
+* we use recursive function as loop where the cycle
+* param correponds of the number of iterations we want to execute
+*
+* @param {Number} iteration corresponds to the number of documents to create
+* @returns {Function} function call insertDocuments()
 */
-const documents = []
 const createDocuments = (cycle) => {
   if (cycle > 0) {
     // const date = new Date() // reference
     // const time = moment(date, 'YYYY-MM-DD HH:mm:ss').add(5, 'days').toDate()
     documents.push({
-      username: faker.name.firstName(),
-      created: faker.date.recent()
+      username: faker.name.firstName(), // add firstName
+      created: faker.date.recent() // add recent date
     })
     return createDocuments(cycle - 1)
   } else {
     console.log('Loop done!')
-    // documents.reverse()
+    // documents.reverse() // reverse array order
     /**
     * Start seeding
     */
-    seed(documents)
+    insertDocuments(documents)
   }
 }
-// loop(100)
+// createDocuments(100)
 // console.log(documents)
 
 /**
-* Start seeding
-*/
+ * Seed db collection
+ *
+ * @param {Number} iteration corresponds to the number of documents to create
+ */
 const seeding = (iteration) => {
   // Connect to db
   connectDB()
@@ -67,9 +83,10 @@ const seeding = (iteration) => {
     if (err) {
       console.log(err)
     } else {
+      // Check id the collection is empty first
       if (users.length) {
         // collection is not emtpy
-        console.log('already seedings')
+        console.log('[MongoDB] already seeded!')
         mongoose.connection.close()
         console.log('[MongoDB] connection closed!')
       } else {
@@ -78,15 +95,20 @@ const seeding = (iteration) => {
         /**
         * Create documents
         */
+        // @description         call createDocuments function
+        // @method              createDocuments()
         createDocuments(iteration) // create users document
       }
     }
   })
 }
-seeding(10) // define number of document to create the start creation
+seeding(10) // start
+
 /**
 * Seeding only One User
 */
+// @description         insert one document in db
+// @method              .save()
 // eslint-disable-next-line no-unused-vars
 const seedingOne = () => {
   User.find((err, seedings) => {
