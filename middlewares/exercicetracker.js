1123bf710be6bc26e12297fe60ea1f4e7ce2c3a0
Drozerah <drozerah@gmail.com>
2020-01-28 16:25:50 +0100

    feat: /api/exercise/add add validation

--- middlewares/exercicetracker.js
+++ middlewares/exercicetracker.js
@@ -6,6 +6,8 @@ const { body, query, validationResult } = require('express-validator')
 * Mongoose Helpers
 */
 const { TypesObjectIdisValid } = require('../utils/helpers/mongoDB')
+const { APICustomErrors } = require('../utils/helpers/APIcustomErrors')
+const { User } = require('../models/exercicetracker') // username and exercice db model
 
 /**
 * Validate request parameters for POST /api/exercise/new-user route
@@ -72,7 +74,7 @@ const validationRulesLog = [
 ]
 const validationTasksLog = (req, res, next) => {
   // console.log('validate user middleware') //! DEBUG
-  // * get validation result
+  // * get validation results
   const Result = validationResult(req)
   if (!Result.isEmpty()) {
     // console.log(Result.array({ onlyFirstError: true })[0]) //! DEBUG
@@ -97,9 +99,72 @@ const validationTasksLog = (req, res, next) => {
     next()
   }
 }
+/**
+* Validate URL query parameters for GET /api/exercise/add route
+*
+* @description     URL parameters validation
+* @route           GET /api/exercise/add
+*/
+const validationRulesAdd = [
+  body('userId')
+    .exists().withMessage('parameter is required')
+    .bail()
+    .custom(value => TypesObjectIdisValid(value) === true).withMessage('invalid value')
+    .bail()
+    .custom(async value => {
+      const user = await User.exists({ _id: value })
+      if (!user) {
+        return Promise.reject(user)
+      }
+    }).withMessage('invalid value'),
+  body('description')
+    .exists().withMessage('parameter is required')
+    .bail()
+    .isLength({ min: 3 }).withMessage('parameter must be at least 3 characters long')
+    .isLength({ max: 20 }).withMessage('parameter value maximum length is 20 characters'),
+  body('duration')
+    .exists().withMessage('parameter is required')
+    .bail()
+    .isNumeric().withMessage('parameter must be a numeric value')
+    .bail()
+    .custom(value => {
+      console.log(typeof value)
+      if (Number(value) < 1 || Number(value) > 1440) {
+        return false
+      } else {
+        return true
+      }
+    }).withMessage('parameter must be a number between 1 and 1440')
+]
+const validationTasksAdd = async (req, res, next) => {
+  // * get validation result
+  const Result = await validationResult(req)
+  if (!Result.isEmpty()) {
+    // console.log(Result.array({ onlyFirstError: true })[0]) //! DEBUG
+    // eslint-disable-next-line dot-notation
+    // console.log(Result['errors']) //! DEBUG
+    const response = APICustomErrors.assign(400)
+    response.details = []
+    // eslint-disable-next-line dot-notation
+    Result['errors'].forEach(element => {
+      const obj = {}
+      obj.value = typeof element.value === 'undefined' ? 'undefined' : element.value
+      obj.msg = element.msg
+      obj.param = element.param
+      // validation.details.push(obj)
+      response.details.push(obj)
+    })
+    // console.log(response) //! DEBUG
+    res.status(response.error.code).json(response)
+  } else {
+    next()
+  }
+}
 module.exports = {
   validationRulesNewUser,
   validationTasksNewUser,
   validationRulesLog,
-  validationTasksLog
+  validationTasksLog,
+  validationRulesAdd,
+  validationTasksAdd
 }
